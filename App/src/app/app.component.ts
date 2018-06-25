c4d5bb296b62e49e6d52bbe0a45a8ce91e16015f
marcelo nieto <marcechelo25@hotmail.com>
2018-06-25 10:58:40 -0500

    binding

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -14,4 +14,21 @@ export class AppComponent {
   arregloNumeros = [5,4,3,2,1];
   mostrarTitulo = true;
 
+  imagenUrl ='https://images.pexels.com/photos/39811/pexels-photo-39811.jpeg?auto=compress&cs=tinysrgb&h=350';
+  imagenWidth = 200;
+
+  html = `
+    <h1> Hola </h1> 
+    <p>Hola mundo</p>
+    <img src="${this.imagenUrl}" alt="">  
+  `;
+
+  aumentarWidht(evento){
+    this.imagenWidth +=20;
+    console.log(evento)
+  }
+  restarWidht(){
+    this.imagenWidth -=20;
+  }
+
 }
