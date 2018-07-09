5714b309a8bef77227b668d437abac212414e4eb
marcelo nieto <marcechelo25@hotmail.com>
2018-07-09 10:59:32 -0500

    Formularios

---
+++ Formularios/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  email;
+  password;
+  passwordConfirmation;
+
+  ingresarUsuario(formulario) {
+
+    const valorActualPassword = formulario
+      .controls.password.value;
+    const valorActualPasswordConfirmation = formulario.controls.passwordConfirmation.value;
+
+    if (valorActualPassword === valorActualPasswordConfirmation) {
+      alert('BIEN!');
+    } else {
+      alert('Los passwords no son iguales');
+      this.password = undefined;
+      this.passwordConfirmation = undefined;
+    }
+
+  }
+}
